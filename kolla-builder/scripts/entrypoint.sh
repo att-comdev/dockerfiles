77c724e906ba4c3cc34f99f7070b098025c4284b
Brandon B. Jozsa <bjozsa@jinkit.com>
2016-12-13 14:43:42 -0500

    automatic kolla-builds

--- kolla-builder/scripts/entrypoint.sh
+++ kolla-builder/scripts/entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+# Allow for debugging first:
+if [ "$1" = "bash" ]; then
+  exec bash
+fi
+
+# Start Supervisord
 sudo /usr/bin/supervisord
 sleep 2
+
+# Activate the kolla-build environment:
 . /root/.venv/kolla-builds/bin/activate
-exec bash $*
+
+# Log into your registy:
+docker login -u="$DOCKER_USER" -p="$DOCKER_PASS" $DOCKER_REGISTRY
+
+# Attempt to run kolla-build on container entry:
+kolla-build $KOLLA_PROJECT 
+
+### exec bash $*
+# END
