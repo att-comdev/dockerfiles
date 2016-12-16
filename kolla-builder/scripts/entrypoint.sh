328ecd88b3c7742a20bc4f1e828c6a23805197ec
Brandon B. Jozsa <bjozsa@gmail.com>
2016-12-15 22:48:04 -0500

    reconfigure entrypoint

--- kolla-builder/scripts/entrypoint.sh
+++ kolla-builder/scripts/entrypoint.sh
@@ -11,8 +11,15 @@ sleep 2
 # Activate the kolla-build environment:
 . /root/.venv/kolla-builds/bin/activate
 
+# Add custom values to /etc/kolla/kolla-build.conf:
+crudini --set /etc/kolla/kolla-build.conf DEFAULT base $KOLLA_BASE && \
+crudini --set /etc/kolla/kolla-build.conf DEFAULT install_type $KOLLA_TYPE && \
+crudini --set /etc/kolla/kolla-build.conf DEFAULT threads 1 && \
+crudini --set /etc/kolla/kolla-build.conf DEFAULT namespace "${DOCKER_REGISTRY}/${KOLLA_NAMESPACE}" && \
+crudini --set /etc/kolla/kolla-build.conf DEFAULT tag $KOLLA_TAG
+
 # Clean up any space previously left from docker builds:
-/usr/local/bin/clean.sh
+. /usr/local/bin/clean.sh
 
 # Log into your registy:
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASS" $DOCKER_REGISTRY
@@ -21,7 +28,6 @@ docker login -u="$DOCKER_USER" -p="$DOCKER_PASS" $DOCKER_REGISTRY
 kolla-build $KOLLA_PROJECT 
 
 # TEST ONLY - Push completed containers to the container registry (rework for python/golan):
-/usr/local/bin/kolla-push.sh
+. /usr/local/bin/kolla-push.sh
 
-### exec bash $*
 # END
--- kolla-builder/scripts/kolla-push.sh
+++ kolla-builder/scripts/kolla-push.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-docker images | grep "$KOLLA_NAMESPACE/$KOLLA_BASE-$KOLLA_TYPE-" | cut -d ' ' -f 1 | while read image ; do
-   docker push $image:$KOLLA_TAG
-done
+echo ""
+echo "Preparing to push Kolla images to $DOCKER_REGISTRY:"
+echo "KOLLA_NAMESPACE: $KOLLA_NAMESPACE"
+echo "KOLLA_BASE: $KOLLA_BASE"
+echo "KOLLA_TYPE: $KOLLA_TYPE"
+echo "KOLLA_TAG: $KOLLA_TAG"
+echo "=============================="
+echo ""
+#
+sleep 5
+docker images
+sleep 1
+#
+set -ex
+docker images | grep "${DOCKER_REGISTRY}/${KOLLA_NAMESPACE}/${KOLLA_BASE}-${KOLLA_TYPE}-" | cut -d ' ' -f 1 | while read -r image ; do
+   docker push "${image}:${KOLLA_TAG}"
+done;
