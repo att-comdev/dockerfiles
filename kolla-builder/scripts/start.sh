1d75742332d31082ad2a65309663ae236912f483
Brandon B. Jozsa <bjozsa@gmail.com>
2017-01-22 15:40:09 -0500

    changes to kolla-builder to match lab

--- kolla-builder/scripts/start.sh
+++ kolla-builder/scripts/start.sh
@@ -11,13 +11,6 @@ sleep 2
 # Activate the kolla-build environment:
 . /root/.venv/kolla-builds/bin/activate
 
-# Add custom values to /etc/kolla/kolla-build.conf:
-crudini --set /etc/kolla/kolla-build.conf DEFAULT base $KOLLA_BASE && \
-crudini --set /etc/kolla/kolla-build.conf DEFAULT install_type $KOLLA_TYPE && \
-crudini --set /etc/kolla/kolla-build.conf DEFAULT threads 1 && \
-crudini --set /etc/kolla/kolla-build.conf DEFAULT namespace "${DOCKER_REGISTRY}/${KOLLA_NAMESPACE}" && \
-crudini --set /etc/kolla/kolla-build.conf DEFAULT tag $KOLLA_TAG
-
 # Clean up any space previously left from docker builds:
 . /usr/local/bin/clean.sh
 
@@ -28,6 +21,7 @@ docker login -u="$DOCKER_USER" -p="$DOCKER_PASS" $DOCKER_REGISTRY
 kolla-build $KOLLA_PROJECT 
 
 # TEST ONLY - Push completed containers to the container registry (rework for python/golan):
-. /usr/local/bin/kolla-push.sh
+# REMOVED FOR JENKINS PIPELINE TESTING
+#. /usr/local/bin/kolla-push.sh
 
 # END
