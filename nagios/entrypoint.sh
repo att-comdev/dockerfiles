d766274294eaa187f333fa2433eea993372c97b4
Rakesh Patnaik <patsrakesh@gmail.com>
2018-03-18 16:45:02 +0000

    OSH77 prometheus aware nagios-core4 image
    Change-Id: I30ba768a89e4555c691ea4995787c42d6714b213

---
+++ nagios/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Apply environment variables
+sed -ri -e 's/(^\s+email\s+)\S+(.*)/\1'${NAGIOSADMIN_EMAIL}'\2/' ${NAGIOS_HOME}/etc/objects/contacts.cfg
+sed -i -e 's/nagiosadmin/'${NAGIOSADMIN_USER}'/' ${NAGIOS_HOME}/etc/objects/contacts.cfg
+sed -i -e 's/=nagiosadmin$/='${NAGIOSADMIN_USER}'/' ${NAGIOS_HOME}/etc/cgi.cfg
+sed -ri -e 's/.*USER1.*/$USER1$=\/usr\/lib\/nagios\/plugins/g' ${NAGIOS_HOME}/etc/resource.cfg
+
+if [ ! -f ${NAGIOS_HOME}/etc/htpasswd.users ] ; then
+  htpasswd -bc ${NAGIOS_HOME}/etc/htpasswd.users ${NAGIOSADMIN_USER} "${NAGIOSADMIN_PASS}"
+  chown -R ${NAGIOS_USER}:${NAGIOS_USER} ${NAGIOS_HOME}/etc/htpasswd.users
+fi
+
+/etc/init.d/apache2 restart
+/etc/init.d/nagios restart
+exec /usr/local/bin/nagios_config_discovery_bot.py -d --prometheus_api $PROMETHEUS_SERVICE --object_file_loc /opt/nagios/etc/objects/prometheus_discovery_objects.cfg
